57a5c24d5a7e17b2af3283c73eea371edd815172
lymakara-dev <ly.makara.persnoal@gmail.com>
2025-03-01 00:11:54 +0700

    add create map dto entity

--- src/map/dto/create-map.dto.ts
+++ src/map/dto/create-map.dto.ts
@@ -1,1 +1,23 @@
-export class CreateMapDto {}
+import { IsString, IsNotEmpty, IsOptional, IsNumber } from 'class-validator';
+
+export class CreateMapDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string; // Name of the map
+
+  @IsString()
+  @IsOptional()
+  description?: string; // Optional description
+
+  @IsNumber()
+  @IsNotEmpty()
+  zoomLevel: number; // Zoom level of the map
+
+  @IsNumber()
+  @IsNotEmpty()
+  latitude: number; // Latitude coordinate
+
+  @IsNumber()
+  @IsNotEmpty()
+  longitude: number; // Longitude coordinate
+}
